72bbcdec9d3078bc3b67d799bb6b8a1d1822a2be
Shivam <shivamanand252@gmail.com>
2021-12-19 00:46:35 +0530

    modified

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const mongoURL =
-  "mongodb://localhost:27017/appstrom?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+  "mongodb://localhost:27017/appstrom?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// console.log(process.env.mongoURL);
 
 const connectMongo = () => {
   mongoose.connect(mongoURL, () => {
