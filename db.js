effba00b5d593f20bcfb25ba31592385664f6d00
Shivam <shivamanand252@gmail.com>
2022-01-11 02:10:13 +0530

    modified

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb+srv://shivam:shivam@appstorm.e4ife.mongodb.net/";
+const mongoURL =
+  "mongodb://localhost:27017/appStrom?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// const mongoURL = "mongodb+srv://shivam:shivam@appstorm.e4ife.mongodb.net/";
 // console.log(process.env.mongoURL);
 
 const connectMongo = () => {
